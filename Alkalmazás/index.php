169917e95d07e47279726a88b58e28d177871d5d
unknown <karacsonybalazs01@gmail.com>
2022-10-02 21:30:43 +0200

    Egy egyszerű kezdőlap a login teszteléséhez

---
+++ Alkalmazás/index.php
@@ -0,0 +1,38 @@
+
+<?php
+
+require './userLoggedIn.php';
+
+$userData = $_SESSION['UserData'];
+
+//Kijelentkezés megvalósítása:
+
+if ( isset($_POST['LogoutBtn']) ) {
+    unset($_SESSION['UserData']);
+    session_destroy();
+    header("Location: ./auth.php");
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Front Page</title>
+</head>
+<body>
+    <h1>User Details:</h1>
+
+    <?php foreach ($userData as $key => $value) {
+        echo "<h3>${key}: ${value}</h3>";
+    }?>
+
+    <form action='' method='post'>
+        <button type='submit' name='LogoutBtn'>Logout</button>
+    </form>
+</body>
+</html>
