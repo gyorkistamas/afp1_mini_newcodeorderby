7cae9255705ab2d25d99e2df84e16933fea95d0d
unknown <karacsonybalazs01@gmail.com>
2022-10-02 20:53:41 +0200

    Login működésének továbbfejlesztése

--- Alkalmazás/logic/login.php
+++ Alkalmazás/logic/login.php
@@ -1,7 +1,6 @@
 <?php
 
     require '../dbcon/dbms.php';
-
     if ( isset($_POST['loginEmail']) && !empty($_POST['loginEmail']) && 
          isset($_POST['loginPass']) && !empty($_POST['loginPass']) ) 
     {
@@ -10,10 +9,10 @@
 
         //Felhasználó lekérdezése az adatbázisból:
 
-        // $con = connect('dbname', 'root', '', 3306);
-        // $query = "";
-        // $result = mysqli_query($con, $query);
-        $results = /*mysqli_fetch_all($result);*/ null;
+        $con = connect('review site', 'root', '', 3306);
+        $query = "SELECT * FROM user WHERE email LIKE '".$loginEmail."'";
+        $result = mysqli_query($con, $query);
+        $results = mysqli_fetch_all($result);
 
         if ($results == null) 
         {
@@ -21,21 +20,23 @@
         }
         else 
         {
-
-            print("Bejelentkezés...");
-            
             //Felhasználó adatainak lekérése az adatbázisból a session létrehozásához:
 
-            // $query = "insert the query here!";
-            // $aggregate = mysqli_query($con, $query);
-            // $aggregates = mysqli_fetch_row($aggregate);
+            $query = "SELECT name, email, password, moderator FROM user WHERE email LIKE '".$loginEmail."'";
+            $aggregate = mysqli_query($con, $query);
+            $aggregates = mysqli_fetch_row($aggregate);
 
-            // $email = $aggregates[0];
-            // ...
+            $name = $aggregates[0];
+            $email = $aggregates[1];
+            $password = $aggregates[2];
+            $role = $aggregates[3] == 0 ? "User" : "Moderator";
 
-
+            //Felhasználó adatainak eltárolása egy tömbben és annak továbbadása Session változóként:
             
-            // $_SESSION['email'] = $email;
+            $UserData = array('Name' => $name, 'Email' => $email, 'Password' => $password, 'Role' => $role);
+            session_start();
+            $_SESSION['UserData'] = $UserData;
+            //header("Location: ../index.php");
         }
 
     }
