c8bfa19c15ec3d17f5b9732890c2ead81c99c711
anon <anon@anon.anon>
2022-10-05 01:30:30 +0200

    adatbázis init script átmásolása az átláthatóságért

---
+++ database/db.sql
@@ -0,0 +1,129 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+-- PHP verzió: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `review site`
+--
+CREATE DATABASE IF NOT EXISTS `review site` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+USE `review site`;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `product`
+--
+
+CREATE TABLE `product` (
+`product_id` int(11) NOT NULL,
+`product_name` varchar(20) COLLATE utf8_hungarian_ci NOT NULL,
+`description` varchar(2000) COLLATE utf8_hungarian_ci DEFAULT NULL,
+`added_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `review`
+--
+
+CREATE TABLE `review` (
+`review_id` int(11) NOT NULL,
+`message` varchar(2000) COLLATE utf8_hungarian_ci NOT NULL,
+`added_by` int(11) NOT NULL,
+`product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `user`
+--
+
+CREATE TABLE `user` (
+`user_id` int(11) NOT NULL,
+`name` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
+`email` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
+`password` varchar(35) COLLATE utf8_hungarian_ci NOT NULL,
+`moderator` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `product`
+--
+ALTER TABLE `product`
+ADD PRIMARY KEY (`product_id`),
+ADD KEY `fk_user_id` (`added_by`);
+
+--
+-- A tábla indexei `review`
+--
+ALTER TABLE `review`
+ADD PRIMARY KEY (`review_id`),
+ADD KEY `fk_user_id_2` (`added_by`),
+ADD KEY `fk_product_id` (`product_id`);
+
+--
+-- A tábla indexei `user`
+--
+ALTER TABLE `user`
+ADD PRIMARY KEY (`user_id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `product`
+--
+ALTER TABLE `product`
+MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `review`
+--
+ALTER TABLE `review`
+MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `user`
+--
+ALTER TABLE `user`
+MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `product`
+--
+ALTER TABLE `product`
+ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`added_by`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Megkötések a táblához `review`
+--
+ALTER TABLE `review`
+ADD CONSTRAINT `fk_product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `fk_user_id_2` FOREIGN KEY (`added_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
