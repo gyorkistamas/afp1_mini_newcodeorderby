6898f20bf62d8b3be5a7d744714555dba6c0cf00
Tamás Györkis <tamasgyorkis@gmail.com>
2022-10-04 13:02:12 +0200

    favicon hozzáadása többi oldalhoz

--- Alkalmazás/view/regfeedback.php
+++ Alkalmazás/view/regfeedback.php
@@ -18,6 +18,7 @@ $error = isset($_GET['error']) ? $_GET['error'] : "Ismeretlen hiba";
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+	<link rel="icon" type="image/x-icon" href="../imgs/favicon.png">
 	<link rel="stylesheet" type="text/css" href="../bootstrap,jquery/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="../styles/global.css">
     <script src="../bootstrap,jquery/jquery-3.6.1.min.js"></script>
