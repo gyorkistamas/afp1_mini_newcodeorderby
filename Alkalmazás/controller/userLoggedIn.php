4173cae11d7189c8d9629360459aae253560d722
unknown <karacsonybalazs01@gmail.com>
2022-10-06 08:19:26 +0200

    Kód egyszerűsítése

--- Alkalmazás/controller/userLoggedIn.php
+++ Alkalmazás/controller/userLoggedIn.php
@@ -3,14 +3,7 @@
 session_start();
 
 function isLoggedIn(): bool {
-    return !isset($_SESSION['UserData']);
-}
-
-$status = isLoggedIn();
-
-if ($status) {
-    session_destroy();
-    header("Location: ../view/auth.php");
+    return isset($_SESSION['UserData']);
 }
 
 ?>
