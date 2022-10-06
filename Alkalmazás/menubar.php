c3adf7d118b7023785fec2ba64de8d8616283aea
unknown <karacsonybalazs01@gmail.com>
2022-10-06 08:18:38 +0200

    Menü megvalósítása

---
+++ Alkalmazás/menubar.php
@@ -0,0 +1,27 @@
+<?php
+    // require './controller/userLoggedIn.php';
+    // $status = isLoggedIn();
+
+    $route = explode("/", $_SERVER['PHP_SELF']);
+    $thisPage = end($route);
+
+    echo "<form action='' method='post'>";
+
+    echo $status ? "<button type='submit' name='LogoutBtn' class='btn btn-success'>Kijelentkezés</button>
+    <button type='submit' name='AddProductBtn' class='btn btn-success'>Termék hozzáadása</button> ": 
+        "<button type='submit' name='LoginBtn' class='btn btn-success'>Bejelentkezés/Regisztráció</button>";
+
+    // if ($status) {
+        
+    //     echo "<button type='submit' name='LogoutBtn' class='btn btn-success'>Kijelentkezés</button>";
+ 
+    // }
+    // else {
+
+    //     echo "<button type='submit' name='LoginBtn' class='btn btn-success'>Bejelentkezés/Regisztráció</button>";
+                
+    // }
+        echo "<button type='submit' name='HomePageBtn' class='btn btn-success'>Főoldal</button>";
+    echo "</form>";
+?>
+
