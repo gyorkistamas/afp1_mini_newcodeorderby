7d0edf6cd162e437e5005987545b70a6e4a55f31
unknown <karacsonybalazs01@gmail.com>
2022-10-08 11:49:06 +0200

    Menü megjelenésének fixálása

--- Alkalmazás/menubar.php
+++ Alkalmazás/menubar.php
@@ -1,32 +1,29 @@
 <?php
     // require './controller/userLoggedIn.php';
-    // $status = isLoggedIn();
+    $status = isLoggedIn();
 
     $route = explode("/", $_SERVER['PHP_SELF']);
     $thisPage = end($route);
 
     $styleAttr = "margin-top: 1%";
 
+    if ( isset($_POST['HomePageBtn']) ) {
+        header("Location: ../index.php");
+    }
+
+    if ( isset($_POST['AddProductBtn']) ) {
+        header("Location: view/addProduct.php");
+    }
+
     echo "<form action='' method='post'>";
+
         echo "<button style='".$styleAttr."' type='submit' name='HomePageBtn' class='btn btn-success'"
         .($thisPage == 'index.php' ? 'disabled' : '').">Főoldal</button> ";
     
         echo $status ? "<button style='".$styleAttr."' type='submit' name='AddProductBtn' class='btn btn-primary'".($thisPage == 'addProduct.php' ? 'disabled' : '').">Termék hozzáadása</button> 
-        <button style='".$styleAttr."' type='submit' name='LogoutBtn' class='btn btn-danger'>Kijelentkezés</button>": 
+            <button style='".$styleAttr."' type='submit' name='LogoutBtn' class='btn btn-danger'>Kijelentkezés</button>": 
                 "<button style='".$styleAttr."' type='submit' name='LoginBtn' class='btn btn-success'>Bejelentkezés/Regisztráció</button>";
-
-    // if ($status) {
-        
-    //     echo "<button type='submit' name='LogoutBtn' class='btn btn-success'>Kijelentkezés</button>";
- 
-    // }
-    // else {
-
-    //     echo "<button type='submit' name='LoginBtn' class='btn btn-success'>Bejelentkezés/Regisztráció</button>";
-                
-    // }
-       
-        // echo "<h1 class='text-white'>".$thisPage."</h1>";
+    
     echo "</form>";
 ?>
 
