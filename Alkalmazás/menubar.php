7061300e43e50e8e1dcaa99b27046e35ac5e7a7b
unknown <karacsonybalazs01@gmail.com>
2022-10-08 17:56:17 +0200

    Gombok dizájnolása 2.0

--- Alkalmazás/menubar.php
+++ Alkalmazás/menubar.php
@@ -1,5 +1,4 @@
 <?php
-    // require './controller/userLoggedIn.php';
     $status = isLoggedIn();
 
     $route = explode("/", $_SERVER['PHP_SELF']);
@@ -17,12 +16,12 @@
 
     echo "<form action='' method='post'>";
 
-        echo "<button style='".$styleAttr."' type='submit' name='HomePageBtn' class='btn btn-success'"
+        echo "<button style='".$styleAttr."; color: DarkSlateGray;' type='submit' name='HomePageBtn' class='btn btn-info'"
         .($thisPage == 'index.php' ? 'disabled' : '').">Főoldal</button> ";
     
-        echo $status ? "<button style='".$styleAttr."' type='submit' name='AddProductBtn' class='btn btn-primary'".($thisPage == 'addProduct.php' ? 'disabled' : '').">Termék hozzáadása</button> 
+        echo $status ? "<button style='".$styleAttr."; color: DarkSlateGray;' type='submit' name='AddProductBtn' class='btn btn-info'".($thisPage == 'addProduct.php' ? 'disabled' : '').">Termék hozzáadása</button> 
             <button style='".$styleAttr."' type='submit' name='LogoutBtn' class='btn btn-danger'>Kijelentkezés</button>": 
-                "<button style='".$styleAttr."' type='submit' name='LoginBtn' class='btn btn-success'".($thisPage == 'auth.php' ? 'disabled' : '').">Bejelentkezés/Regisztráció</button>";
+                "<button style='".$styleAttr."; color: DarkSlateGray;' type='submit' name='LoginBtn' class='btn btn-info'".($thisPage == 'auth.php' ? 'disabled' : '').">Bejelentkezés/Regisztráció</button>";
     
     echo "</form>";
 ?>
