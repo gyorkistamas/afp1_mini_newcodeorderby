58dd490c631845a8a3b350308cadd9e1d14e619e
anon <anon@anon.anon>
2022-10-09 12:06:34 +0200

    loginPopup.php - symbol rename; mv

--- Alkalmazás/view/loginPopup.php
+++ Alkalmazás/view/loginPopup.php
@@ -3,6 +3,6 @@
 	<p>Vélemény közzé tételre csak bejelentkezés után van lehetőség. Kérem jelentkezzen be vagy regisztráljon!</p>
 	<div>
 		<button class=rounded onClick="window.location = 'index.php'">Bejenetkezés</button>
-		<button class=rounded onClick="login_popup_die();">Mégsem</button>
+		<button class=rounded onClick="LoginPopupDie();">Mégsem</button>
 	<div>
 </div>
